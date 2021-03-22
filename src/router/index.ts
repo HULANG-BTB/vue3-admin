2274338c334d867293a4df4f13de517930eef8e1
AloneH <admin@oibit.cn>
2021-03-22 15:34:25 +0800

    feat: 构造递归菜单数据

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,73 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 
-const routes: Array<RouteRecordRaw> = []
+import BaseLayout from '@/layout/base-layout/index.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    component: BaseLayout
+  },
+  {
+    path: '/nested',
+    component: BaseLayout,
+    redirect: '/nested/menu1/menu1-1',
+    name: 'Nested',
+    meta: {
+      title: 'Nested Routes',
+      icon: 'nested'
+    },
+    children: [
+      {
+        path: 'menu1',
+        component: () => import('@/views/nested/menu1/index.vue'), // Parent router-view
+        name: 'Menu1',
+        meta: { title: 'Menu 1' },
+        redirect: '/nested/menu1/menu1-1',
+        children: [
+          {
+            path: 'menu1-1',
+            component: () => import('@/views/nested/menu1/menu1-1/index.vue'),
+            name: 'Menu1-1',
+            meta: { title: 'Menu 1-1' }
+          },
+          {
+            path: 'menu1-2',
+            component: () => import('@/views/nested/menu1/menu1-2/index.vue'),
+            name: 'Menu1-2',
+            redirect: '/nested/menu1/menu1-2/menu1-2-1',
+            meta: { title: 'Menu 1-2' },
+            children: [
+              {
+                path: 'menu1-2-1',
+                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1/index.vue'),
+                name: 'Menu1-2-1',
+                meta: { title: 'Menu 1-2-1' }
+              },
+              {
+                path: 'menu1-2-2',
+                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2/index.vue'),
+                name: 'Menu1-2-2',
+                meta: { title: 'Menu 1-2-2' }
+              }
+            ]
+          },
+          {
+            path: 'menu1-3',
+            component: () => import('@/views/nested/menu1/menu1-3/index.vue'),
+            name: 'Menu1-3',
+            meta: { title: 'Menu 1-3' }
+          }
+        ]
+      },
+      {
+        path: 'menu2',
+        name: 'Menu2',
+        component: () => import('@/views/nested/menu2/index.vue'),
+        meta: { title: 'Menu 2' }
+      }
+    ]
+  }
+]
 
 const router = createRouter({
   history: createWebHashHistory(),
