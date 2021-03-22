0c0615152f4e3cacc42445239f5154daef10a6cb
AloneH <admin@oibit.cn>
2021-03-22 15:47:03 +0800

    feat: ElementPlus依赖引入和组件注册

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'normalize.css/normalize.css'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+
 const app = createApp(App)
 app.use(store)
 app.use(router)
+app.use(ElementPlus)
 app.mount('#app')
